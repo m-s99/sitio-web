076f961e6904711a48d601d756e9afa3a66c3f3a
matiassabino1905 <matiassabino1905@gmail.com>
2019-09-16 22:56:25 -0300

    update 22:56

--- js/codigo.js
+++ js/codigo.js
@@ -2,5 +2,14 @@ $(document).ready(
     function ()
     {
               $("#fondo").fadeIn(3000);
+              $("#tituloh4").bind("click", mostrarOcultarTitulo);
     }
+
     );
+
+    function mostrarOcultarTitulo()
+    {
+        $("#TituloIndex").fadeToggle();
+
+    }
+    
